5d7ac10001bab1a07f4d059547eddef34958b723
Kailas Mahalinga Upadhyaya <kailas.m.upadhyaya@fau.de>
2023-11-10 14:43:09 +0100

    Error handling

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='math_quiz_game',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'random',  # No need to specify 'random' as it's part of the standard library
-    ],
     entry_points={
         'console_scripts': [
             'math_quiz = your_module_name:math_quiz',  # Replace with the actual name of your module
